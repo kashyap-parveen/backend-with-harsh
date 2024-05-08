a66636409ddc29085a8963f6643c808f3a80bcc3
Parveen Kashyap <xmen.haryana@gmail.com>
2024-05-08 14:45:24 +0530

    ejs setup

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import 'dotenv/config'
+
+const app  = express()
+
+const port = process.env.PORT || 3000
+app.set("view engine",'ejs');
+
+app.get("/test",(req,res)=>{
+    res.render("index")
+})
+app.get("/",(req,res)=>{
+    res.send("test files 1")
+})
+
+app.listen(port,()=>{
+    console.log(`Your server is running PORT no ${port}`);
+})
